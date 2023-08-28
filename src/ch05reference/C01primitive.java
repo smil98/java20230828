0981542c532cceb87824789c18a5a1825a93ed80
smil98 <seungmilee98@gmail.com>
2023-08-28 11:43:16 +0900

    first

---
+++ src/ch05reference/C01primitive.java
@@ -0,0 +1,15 @@
+package ch05reference;
+
+public class C01primitive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a; //변수 선언
+			a = 3; //타입(자료형)에 맞는 값만 대입 가능
+//			a = 3.14; //타입에 맞지 않다면 대입 불가능
+			
+			int b = 5; //선언과 동시에 값 대입
+			//타입 별 몇 바이트 차지하는지 외울 수 있도록 하기
+	}
+
+}
---
+++ src/ch05reference/C02reference.java
@@ -0,0 +1,23 @@
+package ch05reference;
+
+public class C02reference {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] a = {{1,2,3}, {10,20,30},{100,200,300} };
+		int[][] b = a;
+		
+		System.out.println(a[0][2]);
+		System.out.println(b[0][2]);
+		
+		b[2] = new int[] {111,222,333};
+		System.out.println(b[2][2]);
+		System.out.println(a[2][2]);
+		
+		b = new int[][] {{33,44}, {22,11}};
+		System.out.println(b[0][0]);
+		System.out.println(a[0][0]);
+		
+	}
+
+}
