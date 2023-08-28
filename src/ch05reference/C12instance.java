93ffed7c3f8a21672b7bf64aa7f371a6ba875232
smil98 <seungmilee98@gmail.com>
2023-08-28 15:33:57 +0900

    객체와 클래스

---
+++ src/ch05reference/C12instance.java
@@ -0,0 +1,18 @@
+package ch05reference;
+
+public class C12instance {
+    public static void main(String[] args) {
+        //instance
+        //object
+        //객체:
+        //1. 속성(값, 필드, property, attribute)
+        //2. 기능(메소드, 함수, 액션)
+        /*
+        ex. 자동차(객체)
+        타이어 둘레, 루프 길이, 핸들 재질 등등 = 속성
+        엑셀을 밟거나 기어를 변경 = 기능
+         */
+        //클래스는 객체의 설계도. 어떤 속성과 기능이 있는지 명시해야하기 때문에 클래스는 초기화(생성자), 속성, 필드가 필요
+        System.out.println("");
+    }
+}
