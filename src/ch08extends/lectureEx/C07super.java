c4cf65e3267ae409ac4d8281dd00427c2aebf1bc
smil98 <seungmilee98@gmail.com>
2023-08-29 14:14:38 +0900

    Super

---
+++ src/ch08extends/lectureEx/C07super.java
@@ -0,0 +1,26 @@
+package ch08extends.lectureEx;
+
+public class C07super {
+    public static void main(String[] args) {
+        MySubClass071 o1 = new MySubClass071();
+
+        o1.method1();
+
+    }
+}
+
+class MyClass07 {
+    void method1() {
+        System.out.println("부모 클래스 메소드 가능");
+    }
+}
+
+class MySubClass071 extends MyClass07 {
+    @Override
+    void method1() {
+        super.method1(); //if you want to add additional method onto pre. super
+        System.out.println("자식 클래스의 재정의한 메소드");
+    }
+}
+
+
---
+++ src/ch08extends/lectureEx/C08super.java
@@ -0,0 +1,23 @@
+package ch08extends.lectureEx;
+
+public class C08super {
+    public static void main(String[] args) {
+        MySubClass081 o1 = new MySubClass081();
+
+        o1.method();
+    }
+}
+
+
+class MyClass08 {
+    void method() {
+        System.out.println("\"부모의 메소드!\"");
+    }
+}
+
+class MySubClass081 extends MyClass08 {
+    void method() {
+        System.out.println("\"자식의 메소드!\"");
+        super.method();
+    }
+}
