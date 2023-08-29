97e4b4bac69cd8db8ea9655af567daee09c31bf2
smil98 <seungmilee98@gmail.com>
2023-08-29 12:28:21 +0900

    Override

---
+++ src/ch08extends/lectureEx/C06override.java
@@ -0,0 +1,21 @@
+package ch08extends.lectureEx;
+
+public class C06override {
+    public static void main(String[] args) {
+        MySubClass061 o1 = new MySubClass061();
+        o1.method10();
+    }
+}
+
+class MyClass06 {
+    public void method10() {
+        System.out.println("부모 클래스의 메소드");
+    }
+}
+class MySubClass061 extends MyClass06 {
+    @Override
+    public void method10() {
+        System.out.println("서브 클래스의 메소드");
+    }
+}
+
