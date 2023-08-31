b07fc76794d7fe2dcf7a5c7b3d90af63475b1808
smil98 <seungmilee98@gmail.com>
2023-08-31 10:21:17 +0900

    access modifier example

---
+++ src/ch08extends/bookEx/sec07/exam13/package1/A.java
@@ -0,0 +1,12 @@
+package ch08extends.bookEx.sec07.exam13.package1;
+
+public class A {
+    protected String field;
+
+    protected A() {
+
+    }
+    protected void method() {
+
+    }
+}
---
+++ src/ch08extends/bookEx/sec07/exam13/package1/B.java
@@ -0,0 +1,9 @@
+package ch08extends.bookEx.sec07.exam13.package1;
+
+public class B {
+    public void method() {
+        A a = new A();
+        a.field = "value";
+        a.method();
+    }
+}
