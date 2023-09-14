263ede1a68005f12bd57c5f931057fdaad4e74d9
smil98 <seungmilee98@gmail.com>
2023-09-14 14:44:08 +0900

    Custom Exception and Message

---
+++ src/ch11exception/lecture/C23custom.java
@@ -0,0 +1,16 @@
+package ch11exception.lecture;
+
+public class C23custom {
+    public static void main(String[] args) {
+        int money = 300;
+        if(money < 10000) {
+            throw new LackingMoneyException("No Money");
+        }
+    }
+}
+
+class LackingMoneyException extends RuntimeException {
+    public LackingMoneyException(String message) {
+        super(message);
+    }
+}
