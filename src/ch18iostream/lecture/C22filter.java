2db35893eda0f35b00f73624325daceb9e3cdfcf
smil98 <seungmilee98@gmail.com>
2023-09-15 16:49:27 +0900

    BufferedInputStream, BufferedOutputStream

---
+++ src/ch18iostream/lecture/C22filter.java
@@ -0,0 +1,15 @@
+package ch18iostream.lecture;
+
+import java.io.*;
+
+public class C22filter {
+    public static void main(String[] args) throws Exception {
+        InputStream is = new FileInputStream("");
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+
+        OutputStream os = new FileOutputStream("");
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        OutputStreamWriter osw = new OutputStreamWriter(bos);
+    }
+}
